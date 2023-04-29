da5d65e2fe79ad68dc8ce47588598ff9e914c85e
Sonkar_Shreya <sonkarshreya1@gmail.com>
2023-04-29 22:21:16 +0530

    add assets

--- script.js
+++ script.js
@@ -8,6 +8,27 @@ const app=express();
 // });
 app.set('view engine','ejs');
 app.set('views',path.join(__dirname,'views'));
+app.use(express.urlencoded());
+// app.use(function(req, res, next){
+//     console.log('middleware 1 called');
+//     next()
+// });
+
+
+//middleware
+
+app.use(express.static('assets'));
+
+   
+// app.use(function(req, res, next){    
+//     console.log('middleware 2 called');
+//     next();
+// });
+   
+// app.use(function(req, res, next){    
+//     console.log('middleware 3 called');
+//     next();
+// });
 
 const contactList=[
     {
@@ -32,6 +53,22 @@ app.get('/',function(req,res){
         contact_list:contactList
     });
 })
+app.get('/practice',function(req,res){
+    return res.render('practice',{
+        title:"practice",
+        
+    });
+})
+
+app.post('/create-contact',function(req,res){
+    // contactList.push({
+    //     name:req.body.name,
+    //     phone:req.body.phone
+    // });
+    contactList.push(req.body);
+    return res.redirect('back')
+})
+
 app.listen(port,function(err){
     if(err){
         console.log('Error is running on the server')
